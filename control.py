0b4f0be670cab2f2e93529474beb5f82454e42e3
Your Name <you@example.com>
2025-10-07 12:06:17 +0500

    add back

--- control.py
+++ control.py
@@ -105,7 +105,7 @@ def send_limit_request(new_limit):
                 "0035": {"v": new_limit},
                 "00F3": {"v": "1"},
                 "00CE": {"v": "20"},
-                "00F8": {"v": "0"}  # remove battery usage time of use
+                "00F8": {"v": "1"}  # remove battery usage time of use
             }
         }),
         "orderTimeout": 60
